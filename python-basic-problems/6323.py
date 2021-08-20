c6e9d87c0360c4d99ada189cc96aa0790ff3bf08
LBU <vvookly@gmail.com>
2021-08-19 23:05:39 -0400

    SW EXPERT 6323

--- python-basic-problems/6323.py
+++ python-basic-problems/6323.py
@@ -3,3 +3,21 @@
 # 10
 # 출력
 # [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+# n = int(input())
+# a, b = 1, 1
+# ans = []
+# for i in range(n):
+#     ans.append(a)
+#     a, b = b, a + b
+# print(ans)
+
+# n = 1260
+# count = 0
+
+# coin_types = [500, 100, 50, 10]
+
+# for coin in coin_types:
+#     count += n // coin
+#     n %= coin
+# print(count)
