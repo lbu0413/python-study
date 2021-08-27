35d9ea3e2eaafc5ea0d4c23bcbd1f7b9e77ae844
LBU <vvookly@gmail.com>
2021-08-26 22:43:05 -0400

    python hangman game pt.1

---
+++ chapter6/hangman.py
@@ -0,0 +1,58 @@
+import time
+
+name = input("Please enter your name: ")
+
+print(f"Hi, {name}. It's time to play the game!")
+print()
+
+time.sleep(1)
+print("Start loading..")
+print()
+time.sleep(0.5)
+
+# 정답 단어
+word = "butterfly"
+
+# 추측 단어
+guesses = ""
+
+# 기회
+turns = 10
+
+# 핵심 while loop
+# 찬스 카운트가 남아 있을 경우
+while turns > 0:
+    # 실패 횟수
+    failed = 0
+
+    # 정답 단어 반복
+    for char in word:
+        # 정답 단어 내에 추측 문자가 포함되어 있는 경우
+        if char in guesses:
+            # 추측 단어 출력
+            print(char, end=" ")
+        else:
+            # 틀린 경우
+            print("_", end=" ")
+            failed += 1
+    if failed == 0:  # 정답
+        print()
+        print()
+        print("Congratulations! you guessed the word!")
+        break
+    print()
+    # 추측 단어 문자 단위 입력
+    print()
+
+    guess = input("Guess a character in the word! ")
+    guesses += guess
+
+    # 정답 단어에 추측한 문자가 포험되어 있지 않을 경우
+    if guess not in word:
+        turns -= 1
+        # 오류 메세지
+        print("sorry your guess is not in the word")
+        print(f"You have {turns} guesses remaining")
+
+        if turns == 0:
+            print("You failed to guess the word :( ")
